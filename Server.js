cc41de6c9a82f1bb6a1f895c87e8f913c19061fe
chaucyzhang <chaucyzhang@gmail.com>
2013-02-03 23:17:07 -0500

    updated

--- Server.js
+++ Server.js
@@ -4,10 +4,10 @@
     var serverAction =function(request,response){
     console.log("request received");
     response.writeHead(200,{"Content-Type":"text-plain"});
-    response.write("hello world");
+    response.write("Hello lolita");
     response.end();
   }
     http.createServer(serverAction).listen(process.env.PORT,process.env.IP);
-    console.log("server started");
+    console.log("server start");
   }
   exports.start=start;
